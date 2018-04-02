de62ad358d4498101e6c802da9fad649860b272e
Jake Dubin <jdub4237@yahoo.com>
2018-04-02 16:30:44 -0500

    adding in some geemusic utils files

--- skill-google-music/__init__.py
+++ skill-google-music/__init__.py
@@ -54,16 +54,8 @@ class GooglePlayMusicSkill(MycroftSkill):
             self.count -= 1
         self.speak_dialog("count.is.now", data={"count": self.count})
 
-    # The "stop" method defines what Mycroft does when told to stop during
-    # the skill's execution. In this case, since the skill's functionality
-    # is extremely simple, there is no need to override it.  If you DO
-    # need to implement stop, you should return True to indicate you handled
-    # it.
-    #
-    # def stop(self):
+    def stop(self):
     #    return False
 
-# The "create_skill()" method is used to create an instance of the skill.
-# Note that it's outside the class itself.
 def create_skill():
-    return TemplateSkill()
+    return GooglePlayMusicSkill()
